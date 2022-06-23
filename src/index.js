c6fca55308dcf2a2cffbd98a2522f96d7b2beca7
PaulSensor <sensor445@gmail.com>
2022-06-23 23:54:00 +0300

    feat: add solution task Reverse Int in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    const arrN = Math.abs(n).toString().split('').reverse()
+    for (let i = 0; i < arrN.length; i++) {
+        if (arrN[i] == 0) {
+            continue
+        } else {
+            return +arrN.splice(i).join('')
+        }
+    }
 }
